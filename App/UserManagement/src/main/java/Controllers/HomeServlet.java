0d98b40f11ebd5dfd61d063203fb6d38604f9590
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2021-01-30 19:24:51 +0200

    Update

--- App/UserManagement/src/main/java/Controllers/HomeServlet.java
+++ App/UserManagement/src/main/java/Controllers/HomeServlet.java
@@ -1,5 +1,9 @@
 package Controllers;
 
+import Repository.DataRepo;
+import Service.UserService;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +13,8 @@ import java.io.IOException;
 
 @WebServlet("/")
 public class HomeServlet extends HttpServlet {
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             resp.getWriter()
